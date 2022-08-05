d306c819a0393e5b1183051bb2ea4a1e42065ee8
Ghilain <ishimweghilain5050@gmail.com>
2022-08-05 10:40:30 +0200

    add factorial method in Slover class

--- solver.rb
+++ solver.rb
@@ -1,3 +1,13 @@
 class Solver
+  def factorial(num)
+    if num.negative?
+     'Please enter positive number or equal to 0!' # rubocop:todo Layout/IndentationWidth
 
+    elsif [0, 1].include?(num)
+       1 # rubocop:todo Layout/IndentationWidth
+    else
+
+   num * factorial(num - 1) # rubocop:todo Layout/IndentationWidth
+    end
+  end
 end
